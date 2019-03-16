be723cc24b6a320fca138c12460c8656499a46c0
Andres Alba <andres_aalba@hotmail.com>
2019-03-15 20:34:37 -0500

    Add CSS, JS and image serving (Lecture 46)

--- src/app.js
+++ src/app.js
@@ -1,24 +1,9 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>');
-});
-
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Andrew',
-        age: 27
-    }, {
-        name: 'Sarah',
-        age: 22
-    }]);
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
-});
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/weather', (req, res) => {
     res.send({

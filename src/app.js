11db86a33d34dd89c23192a394865c2cff2ec4e1
Andres Alba <andres_aalba@hotmail.com>
2019-03-18 10:30:23 -0400

    Add dynamic pages using handlebars - hbs (Lecture 47)

--- src/app.js
+++ src/app.js
@@ -5,6 +5,28 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Andrew Mead'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpfull text'
+    });
+});
+
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is showing',
